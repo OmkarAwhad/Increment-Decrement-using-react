49feede4644a6ed5d475ed4c30666038258a49dc
omkar awhad <omkarawhad3007@gmail.com>
2024-04-13 16:44:46 +0530

    responsiveness

--- src/Components/IncrementDecrement.jsx
+++ src/Components/IncrementDecrement.jsx
@@ -17,15 +17,15 @@ function IncrementDecrement(){
     }
 
     return(
-        <div className=" w-[20%] mx-auto text-center my-auto flex flex-col gap-10 " >
-            <h1 className="text-3xl font-bold text-blue-400 ">Increment & Decrement</h1>
-            <div className="w-[50%] h-18 text-[40px] bg-white mx-auto  ">
-                <div className="flex h-[80%] justify-evenly items-center py-2">
+        <div className=" w-[70%] lg:w-[20%] mx-auto text-center my-auto flex flex-col gap-10  " >
+            <h1 className="text-base font-bold text-blue-400 lg:text-3xl">Increment & Decrement</h1>
+            <div className="w-[70%] h-18 text-[40px] bg-white mx-auto lg:w-[50%] ">
+                <div className="flex h-[80%] justify-evenly items-center  py-2">
 
                     {/* // minus */}
                     <button onClick={minusHandler} className=" border-r-4 border-black ml-2 pr-4">-</button>
 
-                    <button>{count}</button>
+                    <button className="px-2 ">{count}</button>
 
                     {/* // plus */}
                     <button onClick={plusHandler} className="border-l-4 border-black pl-4">+</button>
@@ -34,7 +34,7 @@ function IncrementDecrement(){
             </div>
 
             <div>
-                <button onClick={resetHandler} className="bg-blue-400 px-5 py-2 font-bold text-xl ">Reset</button>
+                <button onClick={resetHandler} className="bg-blue-400 px-5 py-2 font-bold text-base lg:text-xl ">Reset</button>
             </div>
         </div>
     )
